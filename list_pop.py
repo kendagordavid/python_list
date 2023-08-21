cac1d0c58b727849738eb190b682fee0df0beb76
davidkendagor <davidkendagor7@gmail.com>
2023-08-21 13:03:43 +0300

    updated pop() list method

---
+++ list_pop.py
@@ -0,0 +1,17 @@
+'''The list pop() method removes the item at the specified index.
+ The method also returns the removed item.'''
+
+music_instruments = ['piano','guitar','drum','violin','cello']
+
+''' remove the element at index 2'''
+removed_element = music_instruments.pop(2)
+'''print'''
+
+print('Removed Element:', removed_element)
+print('Updated List:', music_instruments)
+
+'''Output: 
+Removed Element: drum
+Updated List: ['piano', 'guitar', 'violin', 'cello']'''
+
+#love python
