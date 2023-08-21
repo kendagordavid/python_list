07f7a7e99eceed3f1c675b990d1f863d0e811069
kmathew201@gmail.com <kmathew201@gmail.com>
2023-08-21 14:11:35 +0300

    added remove() list method

---
+++ list_remove.py
@@ -0,0 +1,15 @@
+''''The list remove() method removes the item at the specified index.
+ The method also returns the removed item.'''
+
+# create a list
+animals = ['cat', 'dog', 'rabbit', 'guinea pig']
+
+# 'rabbit' is removed
+animals.remove('')
+
+# Updated animals List
+print('Updated animals list: ', animals)
+
+'''Expected Output 
+Updated animals list:  ['cat', 'dog', 'guinea pig']
+'''
