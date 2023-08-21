5568ceb724e78103953fbaa4bfa6d1263a8682f5
davidkendagor <davidkendagor7@gmail.com>
2023-08-21 12:32:09 +0300

    updated list insert() method

---
+++ list_insert.py
@@ -0,0 +1,12 @@
+'''The insert() method inserts an element to the list at the specified index.'''
+'''Create a list of fruits'''
+fruits = ['orange','apple','lemon','pineapple']
+
+'''The line below will insert a given element to the list'''
+fruits.insert(3,'avocado')
+
+print(fruits)
+
+'''Expected output: ['orange', 'apple', 'lemon', 'avocado', 'pineapple']'''
+
+#love python
