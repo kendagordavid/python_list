7f6212cd10cf4b3c26463ae33e34d558f2eb3a91
davidkendagor <davidkendagor7@gmail.com>
2023-08-21 11:53:49 +0300

    updated list index() method

---
+++ list_index.py
@@ -0,0 +1,11 @@
+'''The index() method returns the specified element in the list'''
+cars = ["Toyota","Mitsubishi","Hyundai","Honda","Mc Laren","jeep"]
+
+'''Lets see the index of "Hyundai"'''
+
+index= cars.index("Hyundai")
+print(index)
+
+'''Expected output: 2'''
+
+#love python
