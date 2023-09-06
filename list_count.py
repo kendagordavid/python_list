adccff1b85a741b44c85bd35cfe8b4e0c5b13f1f
davidkendagor <davidkendagor7@gmail.com>
2023-09-06 16:44:57 +0300

    updated list count method

---
+++ list_count.py
@@ -0,0 +1,16 @@
+'''The count() method returns the number 
+of times the specified element appears in the list.'''
+'''Create a list '''
+
+list = [1,2,3,4,5,6,7,7,8,9]
+'''By writing the line below the code is able to 
+ look for the number of times 7 is in the list'''
+
+list = list.count(6)
+
+print('count of 6 is',list)
+
+'''Expected output: 
+count of 6 is 2 '''
+
+#love python
