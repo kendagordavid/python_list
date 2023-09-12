7bf8ad9628665ee043eee4511a045d4cad129d5d
davidkendagor <davidkendagor7@gmail.com>
2023-09-12 16:41:03 +0300

    updated list clear method

---
+++ list_clear.py
@@ -0,0 +1,8 @@
+'''This clear method is used in clearing all the elements in the list'''
+numbers = [2,3,2,1,4,5,3]
+'''Clear all elements'''
+numbers.clear()
+
+print(numbers)
+
+'''Expected output : []'''
