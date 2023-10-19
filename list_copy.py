bae593d9fb32608586b7d3113a05df1f93ba731a
davidkendagor <davidkendagor7@gmail.com>
2023-10-19 16:52:54 +0300

    updated python list method()

---
+++ list_copy.py
@@ -0,0 +1,9 @@
+'''this method is used to copy items from list'''
+fruits = ['oranges','pineapple','avocado','guava']
+
+fruits = fruits.copy()
+
+print(fruits)
+
+#expected output;['oranges', 'pineapple', 'avocado', 'guava']
+
